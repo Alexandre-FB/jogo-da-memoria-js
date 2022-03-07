216177ac263b0bfab7cb8449b75833628d3dd210
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-03-07 21:49:19 +0000

    :construction: Criado componente VsPlayer
    git status

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,14 +1,15 @@
 import './style.css'
 import PlayerName from '../../components/PlayerName'
+import VsPlayer from '../../components/Vsplayer'
 
 function ScoreBoard(){
     return /*html*/`
         <header class="score-board">
             ${PlayerName("Player1")} 
+            ${VsPlayer()} 
             ${PlayerName("Player2")} 
         </header>
     `;
-
 }
 
 export default ScoreBoard
